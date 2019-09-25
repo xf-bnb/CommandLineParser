e7c9fa5545b9889830f8314de0d3ff3038a0b3a9
Frank Xiong <Frank_Xiong@outlook.com>
2019-09-26 02:04:06 +0800

    update

--- example/example.cpp
+++ example/example.cpp
@@ -26,10 +26,15 @@ int main()
     test_regex({ "0", "01", "123", "-123", "+123", "123.45", "1", "-1", "-1.02", "-0", "123a", "123+45", "+2.003", "+0", "-01", "+1.2", "-2.7", "0.8.7", "1.2.3" },
                "[+-]?(0|[1-9][1-9]*)([.][0-9]+)?");
 
+    const char* const argv[] = { "a", "1", "b", "7" };
 
+    xf::cmd::Parser parser;
+    
+    xf::cmd::Parser::result_t r = parser.Parse(argv, 1, 3);
 
+    const auto& g = r.get();
 
-
+    auto msg = r.get_msg();
 
     return 0;
 }
--- include/cmd_parser.h
+++ include/cmd_parser.h
@@ -38,13 +38,14 @@ namespace xf::cmd
 
     enum state_t {
         state_ok,                   // ok
-        error_unrecognized,         // 无法识别的指令
-        error_duplicated,           // 重复的指令
-        error_missing_required,     // 缺少必需的指令
+        error_nothing,              // 没有任何参数
+        error_unrecognized,         // 无法识别的参数
+        error_duplicated,           // 重复的参数
+        error_missing_required,     // 缺少必需的参数
         error_missing_value,        // 缺少参数
         error_redundant_value,      // 多余的参数值
         error_value_type,           // 参数值类型错误
-        error_non_unique            // 指令过多
+        error_non_unique            // 参数唯一性冲突
     };
 
     struct option_t
@@ -67,115 +68,15 @@ namespace xf::cmd
 
             using variant_t = std::variant<nullptr_t, bool, int, unsigned int, double, string_t>;
 
-            state_t state{ state_ok };
-            string_t msg{ "ok" };
+            state_t state;
+            string_t msg;
             pair_t<string_t, string_t> hint;
             map_t<string_t, string_t> k_map;
             map_t<string_t, variant_t> v_map;
 
-            void _set_error(state_t s, const string_t& key)
-            {
-                state = s;
-                hint.first = key;
-
-                switch (s)
-                {
-                case state_t::error_unrecognized:
-                    msg = R"(error: unrecognized parameter ")" + key + R"(")";
-                    break;
-                case state_t::error_duplicated:
-                    hint.second = k_map[key];
-                    msg = R"(error: repeat paramter ")" + hint.first + R"(" and ")" + hint.second + R"(")";
-                    break;
-                case state_t::error_missing_required:
-                    msg = R"(error: the parameter ")" + key + R"(" must be specified but not found.)";
-                    break;
-                case state_t::error_missing_value:
-                    msg = R"(error: parameter ")" + key + R"(" must have a value.)";
-                    break;
-                case state_t::error_redundant_value:
-                    msg = R"(error: the parameter ")" + key + R"(" doesn't require value.)";
-                    break;
-                case state_t::error_value_type:
-                    msg = R"(error: value-type error of parameter ")" + key + R"(")";
-                    break;
-                case state_t::error_non_unique:
-                    msg = R"(error: extra parameter ")" + key + R"(")";
-                    break;
-                default:
-                    break;
-                }
-            }
-
-            bool _check_key(const string_t& key, const option_t& opt)
-            {
-                auto iter = k_map.find(key);
-                if (iter != k_map.end())
-                {
-                    state = state_t::error_duplicated;
-                    hint = *iter;
-                    msg = "repeat paramter: " + hint.first + " and " + hint.second;
-                    return false;
-                }
-
-                if (!k_map.empty() && opt.check_mode(mode_t::is_unique))
-                {
-                    state = state_t::error_non_unique;
-                    hint.first = key;
-                    msg = "parameter " + key + " can\'t be specified with other parameters.";
-                    return false;
-                }
-
-                return true;
-            }
-
-            template<typename _Type>
-            void _add_value(const string_t& key, const _Type& value, const set_t<string_t>& keys)
-            {
-                v_map.emplace(std::make_pair(key, variant_t(value)));
-                for (auto k : keys) k_map.emplace(std::make_pair(k, key));
-            }
-
-            template<>
-            void _add_value<nullptr_t>(const string_t& key, const nullptr_t& value, const set_t<string_t>& keys)
-            {
-                for (auto k : keys) k_map.emplace(std::make_pair(k, key));
-            }
-
-            void _add_value(const string_t& key, const string_t& value, const set_t<string_t>& keys, const option_t& opt)
-            {
-                switch (opt.vt)
-                {
-                case value_t::vt_string:
-                    _add_value(key, value, keys);
-                    break;
-                case value_t::vt_boolean:
-                    _add_value(key, ('t' == value[0] || 'T' == value[0]), keys);
-                    break;
-                case value_t::vt_float:
-                    _add_value(key, std::stod(value), keys);
-                    break;
-                case value_t::vt_integer:
-                    _add_value(key, std::stoi(value), keys);
-                    break;
-                case value_t::vt_unsigned:
-                    _add_value(key, unsigned int(std::stoul(value)), keys);
-                    break;
-                default:
-                    _add_value(key, nullptr, keys);
-                    break;
-                }
-            }
+            result_t(state_t s, const string_t& text) : state(s), msg(text) { }
 
         public:
-
-            result_t() = default;
-
-            result_t(state_t s, const string_t& text, const pair_t<string_t, string_t>& h)
-                : state(s), msg(text), hint(h)
-            {
-
-            }
 
             bool is_valid() const { return (state_ok == state); }
             bool is_existing(const string_t& key) const { return k_map.find(key) != k_map.end(); }
@@ -212,7 +113,105 @@ namespace xf::cmd
                 }
             }
 
-            auto get() const { return v_map; }
+            const map_t<string_t, variant_t>& get() const { return v_map; }
+
+        private:
+
+            void _set_error(state_t s, const string_t& key)
+            {
+                state = s;
+                hint.first = key;
+
+                switch (s)
+                {
+                case state_t::error_unrecognized:
+                    msg = R"(error: unrecognized parameter ")" + key + R"(")";
+                    break;
+                case state_t::error_duplicated:
+                    hint.second = k_map[key];
+                    msg = R"(error: repeat paramter ")" + hint.first + R"(" and ")" + hint.second + R"(")";
+                    break;
+                case state_t::error_missing_required:
+                    msg = R"(error: the parameter ")" + key + R"(" must be specified but not found.)";
+                    break;
+                case state_t::error_missing_value:
+                    msg = R"(error: parameter ")" + key + R"(" must have a value.)";
+                    break;
+                case state_t::error_redundant_value:
+                    msg = R"(error: the parameter ")" + key + R"(" doesn't require value.)";
+                    break;
+                case state_t::error_value_type:
+                    msg = R"(error: value-type error of parameter ")" + key + R"(")";
+                    break;
+                case state_t::error_non_unique:
+                    msg = R"(error: parameter ")" + key + R"(" can't be specified with other parameters.)";
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            bool _check_key(const string_t& key, const option_t& opt)
+            {
+                auto iter = k_map.find(key);
+                if (iter != k_map.end())
+                {
+                    _set_error(state_t::error_duplicated, key);
+                    return false;
+                }
+
+                if (!k_map.empty() && opt.check_mode(mode_t::is_unique))
+                {
+                    _set_error(state_t::error_non_unique, key);
+                    return false;
+                }
+
+                return true;
+            }
+
+            template<typename _Type>
+            void _add_value(const string_t& key, const _Type& value, const set_t<string_t>& keys)
+            {
+                v_map.emplace(std::make_pair(key, variant_t(value)));
+                for (auto k : keys) k_map.emplace(std::make_pair(k, key));
+            }
+
+            template<>
+            void _add_value<nullptr_t>(const string_t& key, const nullptr_t& value, const set_t<string_t>& keys)
+            {
+                _add_value(key, keys);
+            }
+
+            void _add_value(const string_t& key, const string_t& value, const set_t<string_t>& keys, const option_t& opt)
+            {
+                switch (opt.vt)
+                {
+                case value_t::vt_string:
+                    _add_value(key, value, keys);
+                    break;
+                case value_t::vt_boolean:
+                    _add_value(key, ('t' == value[0] || 'T' == value[0]), keys);
+                    break;
+                case value_t::vt_float:
+                    _add_value(key, std::stod(value), keys);
+                    break;
+                case value_t::vt_integer:
+                    _add_value(key, std::stoi(value), keys);
+                    break;
+                case value_t::vt_unsigned:
+                    _add_value(key, unsigned int(std::stoul(value)), keys);
+                    break;
+                default:
+                    _add_value(key, nullptr, keys);
+                    break;
+                }
+            }
+
+            void _add_value(const string_t& key, const set_t<string_t>& keys)
+            {
+                for (auto k : keys) k_map.emplace(std::make_pair(k, key));
+            }
+
         };
 
         unsigned int group_id{ 0 };
@@ -275,6 +274,9 @@ namespace xf::cmd
 
         result_t Parse(const list_t<string_t>& args) const
         {
+            if (args.empty())
+                return result_t(state_t::error_nothing, R"(error: don't get any parameter.)");
+
             return _Parse(args, { &Parser::_OnKey, &Parser::_OnValue, &Parser::_OnOptional });
         }
 
@@ -287,99 +289,136 @@ namespace xf::cmd
 
         struct less_t {
             bool operator()(const string_t& a, const string_t& b) const {
-                return b.size() < a.size();
+                return ((a.size() == b.size()) ? (a < b) : (b.size() < a.size()));
             }
         };
 
-        using _parse_func_type = size_type (Parser::*)(result_t&, const string_t&, const std::set<string_t, less_t>&, string_t&) const;
+        using _parse_func_type = size_type (Parser::*)(result_t&, const string_t&, const std::set<string_t, less_t>&, string_t&, option_t&) const;
 
         template<size_type n>
         result_t _Parse(const list_t<string_t>& args, const _parse_func_type(&_parse_functions)[n]) const
         {
-            result_t result;
             string_t key;
-            size_type index(0);
+            option_t opt;
+            size_type index(on_key);
             auto keys = _Keys();
+            result_t result(state_t::state_ok, "ok");
 
             for (auto arg : args)
             {
                 if (index < n)
-                    index = (this->*_parse_functions[index])(result, arg, keys, key);
+                    index = (this->*_parse_functions[index])(result, arg, keys, key, opt);
                 else
                     break;
             }
 
+            switch (index)
+            {
+            case on_opt:
+                result._add_value(key, _Keys(key));
+            case on_key:
+                _CheckResult(result);
+                break;
+            case on_value:
+                result._set_error(state_t::error_missing_value, key);
+                break;
+            default:
+                break;
+            }
 
             return result;
         }
 
-        size_type _OnKey(result_t& result, const string_t& arg, const std::set<string_t, less_t>& keys, string_t& k) const
+        size_type _OnKey(result_t& result, const string_t& arg, const std::set<string_t, less_t>& keys, string_t& k, option_t& opt) const
         {
             for (auto key : keys)
             {
                 if (_start_with(arg, key))
                 {
-                    if (arg.size() == key.size())
-                    {
-                        if (result._check_key(key, _Option(key)))
-                        {
-                            k = key;
-                            option_t opt = _Option(key);
-                            if (value_t::vt_nothing == opt.vt)
-                            {
-                                result._add_value(key, nullptr, _Keys(key));
-                                return on_key;
-                            }
+                    if (_is_perfect_match(arg, key))
+                        return _OnPerfectMatch(result, key, k, opt);
 
-                            return (opt.check_mode(mode_t::k_required) ? on_value : on_opt);
-                        }
+                    if (_is_equation(arg, key))
+                        return _OnEquation(result, key, arg.substr(key.size() + 1), _Option(key));
 
-                        return parse_error;
-                    }
-                    else
-                    {
-                        if (arg.size() < (key.size() + 2) || '=' != arg[key.size()])
-                        {
-                            result._set_error(state_t::error_unrecognized, arg);
-                            return parse_error;
-                        }
-
-                        option_t opt = _Option(key);
-                        if (value_t::vt_nothing == opt.vt)
-                        {
-                            result._set_error(state_t::error_redundant_value, key);
-                            return parse_error;
-                        }
-
-                        if (result._check_key(key, opt))
-                        {
-                            string_t value(arg.substr(key.size() + 1));
-                            if (_check_type(value, opt))
-                            {
-                                result._add_value(key, value, _Keys(key), opt);
-                                return on_key;
-                            }
-
-                            result._set_error(state_t::error_value_type, key);
-                            return parse_error;
-                        }
-
-                        return parse_error;
-                    }
+                    break;
                 }
             }
 
-            return false;
+            result._set_error(state_t::error_unrecognized, arg);
+            return parse_error;
         }
 
-        size_type _OnValue(result_t& result, const string_t& arg, const std::set<string_t, less_t>& keys, string_t& k) const
+        size_type _OnValue(result_t& result, const string_t& arg, const std::set<string_t, less_t>& keys, string_t& k, option_t& opt) const
         {
-            return 0;
+            return _OnValueEx(result, k, arg, opt);
         }
 
-        size_type _OnOptional(result_t& result, const string_t& arg, const std::set<string_t, less_t>& keys, string_t& k) const
+        size_type _OnOptional(result_t& result, const string_t& arg, const std::set<string_t, less_t>& keys, string_t& k, option_t& opt) const
         {
-            return 0;
+            for (auto key : keys)
+            {
+                if (_start_with(arg, key))
+                {
+                    if (_is_perfect_match(arg, key))
+                    {
+                        result._add_value(k, _Keys(k));
+                        return _OnPerfectMatch(result, key, k, opt);
+                    }
+
+                    if (_is_equation(arg, key))
+                    {
+                        result._add_value(k, _Keys(k));
+                        return _OnEquation(result, key, arg.substr(key.size() + 1), _Option(key));
+                    }
+
+                    break;
+                }
+            }
+
+            return _OnValue(result, arg, keys, k, opt);
+        }
+
+        size_type _OnPerfectMatch(result_t& result, const string_t& key, string_t& k, option_t& opt) const
+        {
+            opt = _Option(key);
+            if (!result._check_key(key, opt))
+                return parse_error;
+
+            k = key;
+            if (value_t::vt_nothing == opt.vt)
+            {
+                result._add_value(key, nullptr, _Keys(key));
+                return on_key;
+            }
+
+            return (opt.check_mode(mode_t::k_required) ? on_value : on_opt);
+        }
+
+        size_type _OnEquation(result_t& result, const string_t& key, const string_t& value, const option_t& opt) const
+        {
+            if (!result._check_key(key, opt))
+                return parse_error;
+
+            if (value_t::vt_nothing == opt.vt)
+            {
+                result._set_error(state_t::error_redundant_value, key);
+                return parse_error;
+            }
+
+            return _OnValueEx(result, key, value, opt);
+        }
+
+        size_type _OnValueEx(result_t& result, const string_t& key, const string_t& value, const option_t& opt) const
+        {
+            if (_check_type(value, opt))
+            {
+                result._add_value(key, value, _Keys(key), opt);
+                return on_key;
+            }
+
+            result._set_error(state_t::error_value_type, key);
+            return parse_error;
         }
 
         const option_t& _Option(const string_t& key) const
@@ -416,6 +455,42 @@ namespace xf::cmd
             return false;
         }
 
+        bool _CheckResult(result_t& result) const
+        {
+            if (state_t::state_ok == result.get_state())
+            {
+                if (_Option(result.k_map.cbegin()->first).check_mode(mode_t::is_unique))
+                    return true;
+
+                for (auto opt : opt_map)
+                {
+                    if (opt.second.second.check_mode(mode_t::k_required))
+                    {
+                        const string_t& key(*opt.second.first.cbegin());
+                        if (!result.is_existing(key))
+                        {
+                            result._set_error(state_t::error_missing_required, key);
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool _is_perfect_match(const string_t& text, const string_t& key)
+        {
+            return (text.size() == key.size());
+        }
+
+        static bool _is_equation(const string_t& text, const string_t& key)
+        {
+            return ((key.size() + 1) < text.size() && '=' == text[key.size()]);
+        }
+
         static bool _start_with(const string_t& a, const string_t& b)
         {
             return 0 == a.compare(0, b.size(), b);
@@ -442,4 +517,3 @@ namespace xf::cmd
     };
 
 }
-
